6803cc36dff834373c4ceb27e2c2c4b17c5c93bd
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-04-29 14:05:38 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [selectedOption,setSelectedOption] = useState('');
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h2>Parent Component</h2>
+       <div className="child1">
+        <h3>Child Component</h3>
+       <ChildComponent option = "Option 1" onOptionClick = {handleOptionClick} ></ChildComponent>
+       </div>
+       <div className="child2">
+       <h3>Child Component</h3>
+       <ChildComponent option = "Option 2" onOptionClick = {handleOptionClick}></ChildComponent> 
+       </div>
+       <p>selectedOption: {selectedOption}</p>
+    </div>
+  )
+}
+
+function ChildComponent({option,onOptionClick})
+{
+  return(
+    <div>
+      <button onClick={() =>onOptionClick(option)}>{option}</button>
+
     </div>
   )
 }
